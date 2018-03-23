edbe655e7676ecf70b94a7fb0adf0feef87a4223
William Pelletier <william_pelletier@hotmail.ca>
2018-03-23 15:08:05 -0400

    ajout des fonction dans le fichier socket-body.ejs et index.js

--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -6,14 +6,24 @@ module.exports.listen = function(server) {
   let objetUtil = {}
 
   io.on('connection', function(socket) {
-  console.log(socket.id)
-  socket.on('setUser', function(data) {
-    objetUtil[socket.id] = data.user
-    console.log("objetUtil = " + util.inspect(objetUtil))
-    console.log(util.inspect(data))
-    socket.emit('valid_user', data)
-    io.sockets.emit('diffuser_list_user', objetUtil)
-  })
+    console.log(socket.id)
+    socket.on('setUser', function(data) {
+      objetUtil[socket.id] = data.user
+      console.log("objetUtil = " + util.inspect(objetUtil))
+      console.log(util.inspect(data))
+      socket.emit('valid_user', data)
+      io.sockets.emit('diffuser_list_user', objetUtil)
+    })
+
+    socket.on('sendMessage', function(data) {
+      socket.broadcast.emit('message', data);
+      socket.emit('valide_message', data);
+    })
+    
+    socket.on('disconnect', function(data) {
+      delete objetUtil[socket.id];
+      io.sockets.emit('diffuser_list_user', objetUtil);
+    })
 })
  return io
 }
